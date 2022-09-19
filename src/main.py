684ad3004c4f43929e452c488a222e34e3032b63
Shide Foo <shide.foo@gojek.com>
2022-09-19 12:26:38 +0800

    Create parent lookup index

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def chart_finder():
 
 
 def construct_graph(input_charts: List) -> Dict:
-    return {"root": ["a", "b"], "a": ["c", "d"], "b": ["d"], "c": ["e", "f"], "d": ["f"], "e": [], "f": []}
+    return {"root": ["a", "b", "d"], "a": ["c", "d"], "b": ["d"], "c": ["e", "f"], "d": ["f"], "e": [], "f": []}
 
 
 def get_root_chart() -> str:
@@ -18,28 +18,34 @@ def find_dependencies(graph: Dict, root_chart: str):
         dep_list: List = graph[chart]
         if len(dep_list) == 0:
             return []
-        child_dep_set: Set = set()
+        # Stores mapping between child and list of parents
+        child_dep_dict: Dict = {}
         common_dep: List = []
         for d in dep_list:
             child_deps = _f(d)
-            for d in child_deps:
-                if d not in child_dep_set:
-                    child_dep_set.add(d)
+            for d_grand in child_deps:
+                if d_grand not in child_dep_dict:
+                    child_dep_dict[d_grand] = [d]
                 else:
-                    common_dep.append(d)
+                    child_dep_dict[d_grand].append(d)
+                    common_dep.append(d_grand)
         dep_list_set = set(dep_list)
+        new_deps_added = False
         for d in common_dep:
             if d not in dep_list_set:
                 dep_list_set.add(d)
+                new_deps_added = True
         new_dep_list = list(dep_list_set)
         graph[chart] = new_dep_list
         # NOTE:
         # print statement here indicates changes need to be made to chart var Chart.yaml
         # and values.yaml. For example,
-        # "a new deps ['c', 'd', 'f']" indicates
-        # a Chart.yaml dependencies should have f added, a's values.yaml c.f.enabled: False and d.f.enabled: False
-        # This means that the dependant chart needs to be stored
-        print(f"{chart} new deps {new_dep_list}")
+        # "root new deps ['b', 'd', 'a'], parent_look_up={'f': ['a'], 'd': ['a', 'b'], 'c': ['a']}" indicates
+        # root Chart.yaml dependencies
+        # should have deps [a,b,d], root's values.yaml a.d.enabled: False and b.d.enabled: False
+        # d.enabled: True
+        if new_deps_added:
+            print(f"{chart} new deps {new_dep_list}, parent_look_up={child_dep_dict}")
         return new_dep_list
 
     root_dep_list = _f(root_chart)
